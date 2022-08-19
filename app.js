032075b87073b1c482e4da10ef71133029e199be
Julian Calderon <camilo.rodriguez@isolucionSis.onmicrosoft.com>
2022-08-18 21:01:59 -0500

    Console log de indices

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ let alumnos = ["Juan", "Pepe", "Jorge", "Francisco"]
 let indiceJuan = alumnos.indexOf("Juan");
 let indiceFrancisco  = alumnos.indexOf("Francisco");
 
+console.log(indiceJuan);
+console.log(indiceFrancisco);
+
 
 
 
